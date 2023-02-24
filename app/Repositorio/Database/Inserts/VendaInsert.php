970851bd0b060dd2bac1ce6a4916c31bbe9fdcbd
jadsonHub <jadsoneduardo6@gmail.com>
2023-02-24 11:06:43 -0300

    up

--- app/Repositorio/Database/Inserts/VendaInsert.php
+++ app/Repositorio/Database/Inserts/VendaInsert.php
@@ -33,31 +33,6 @@ class VendaInsert
         }
     }
 
-    // codproduto varchar(10),
-    // qtde double,
-    // unitario double,
-    // total decimal(12,2),
-    // subtotal double,
-    // desconto decimal(12,2),
-    // codigo varchar(10),
-    // codnota varchar(10),
-    // codcaixa varchar(10),
-    // cfop varchar(10),
-    // codcliente varchar(10),
-    // codfilial  varchar(10),
-    // data timestamp default current_timestamp,
-    // meio_dinheiro double,
-    // meio_cartaodeb double,
-    // meio_cartaocred double,
-    // meio_chequeap double,
-    // meio_chequeav double,
-    // meio_crediario double,
-    // meio_outros double,
-    // troco double,
-    // valor_recebido double,
-    // valor_produtos double,
-    // total_nota double
-
     protected function prepareInsertSQL($vendas)
     {
         $sql = "
