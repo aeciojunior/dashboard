922a63b5419ce29e718ad802619a97267ee52e7d
jadsonHub <jadsoneduardo6@gmail.com>
2023-03-24 09:52:54 -0300

    up

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -49,12 +49,12 @@
                     </div>
                     <div
                         class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
-                        <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden"
-                            style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signin-ill.jpg');
+                        <div class="position-relative  h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden"
+                            style="background-image: url('{{asset('img/img-login.jpeg')}}');
         background-size: cover;">
-                            <span class="mask bg-gradient-primary opacity-6"></span>
+                            {{-- <span class="mask  opacity-6"></span>
                             <h4 class="mt-5 text-white font-weight-bolder position-relative">"Orbita tecnologia"</h4>
-                            <p class="text-white position-relative">Seu estoque online a onde estiver.</p>
+                            <p class="text-white position-relative">Seu estoque online a onde estiver.</p> --}}
                         </div>
                     </div>
                 </div>
